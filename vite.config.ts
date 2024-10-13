898f22c990399459856b36809ae07dcef3ad7411
Ayesha Athoi <ayeshaathoi62@gmail.com>
2024-10-13 19:14:57 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 
-  server: {
-    port: 3000, // Change this to your desired port
-  },
+  // server: {
+  //   port: 3000, // Change this to your desired port
+  // },
 });
+
