613ae28ce77a7cf56f074188052277bdc4c0a2e3
Ayesha Athoi <ayeshaathoi62@gmail.com>
2024-10-14 00:02:41 +0600

    Revert "Update Instruction.tsx"
    This reverts commit 464b3451f99b2adf4e2461171372aecdf712a4d4.

--- src/Instruction.tsx
+++ src/Instruction.tsx
@@ -50,6 +50,9 @@ function Instruction() {
     <li>
       <strong>Explanation Clarity Score:</strong> How clearly the code review is explained.
     </li>
+    <li>
+      <strong>Scoring on the basis of review size:</strong> Evaluates whether the feedback length is appropriate, concise, and informative.
+    </li>
   </ul>
   <br />
 
