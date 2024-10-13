bc7ca3c03bd6068df53dfa97f18d79ba7e538e97
Ayesha Athoi <ayeshaathoi62@gmail.com>
2024-10-14 00:15:28 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./codeReview/",
+  base: "./",
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
